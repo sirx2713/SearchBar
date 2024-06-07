777d2b80d6ab53490759b30ead7ea312cbfd55b3
Mabende <belindamabende@gmail.com>
2024-06-07 10:17:11 +0200

    Wow, I can do this!

--- SearchBar/ContentView.swift
+++ SearchBar/ContentView.swift
@@ -8,17 +8,53 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+
+    @State private var searchText: String = ""
+
+    enum Month: String, Identifiable, CaseIterable, CustomStringConvertible {
+        var id: UUID {
+            UUID()
         }
-        .padding()
+
+        var description: String {
+            self.rawValue.localizedCapitalized
+        }
+
+        case january, february
+        case march, april, may
+        case june, july, august
+        case september, october, november
+        case december
+    }
+
+    var body: some View {
+        NavigationStack {
+            List {
+                ForEach(
+                    Month.allCases.filter {
+                        searchText.isEmpty || $0.rawValue.contains(searchText.lowercased())
+                    }
+                ) { month in
+                    Text(month.description)
+                }
+            }
+            #if os(macOS)
+            .searchable(text: $searchText, placement: .toolbar)
+            #else
+            .searchable(text: $searchText, placement: .navigationBarDrawer(displayMode: .always))
+            #endif
+            .searchSuggestions {
+                ForEach(Month.allCases) { month in
+                    if month.description.contains(searchText.localizedCapitalized) {
+                        Text(month.description)
+                            .searchCompletion(month.description)
+                    }
+                }
+            }
+            .navigationTitle("Search")
+        }
     }
 }
-
 #Preview {
     ContentView()
 }
