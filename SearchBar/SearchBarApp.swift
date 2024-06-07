0ead4637ffdc3e2880dd06ebacd613afb94b4c37
Mabende <belindamabende@gmail.com>
2024-06-07 10:14:10 +0200

    Initial Commit

---
+++ SearchBar/SearchBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchBarApp.swift
+//  SearchBar
+//
+//  Created by Tafadzwa Alexander Razaro on 2024/06/07.
+//
+
+import SwiftUI
+
+@main
+struct SearchBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
